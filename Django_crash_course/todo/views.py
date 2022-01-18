fe1f33c3e4e890e453d2197e261e69a0e9386bdf
JasleenKaur2000 <jasleenkaur46353@gmail.com>
2022-01-19 02:46:02 +0530

    added CRUD operations in todo list

--- Django_crash_course/todo/views.py
+++ Django_crash_course/todo/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Todo
+from .forms import TodoForm
 
 # Create your views here.
 def todo_list(request):
@@ -10,3 +11,40 @@ def todo_list(request):
     }
     return render(request,"todo-list2.html",context)
 
+#crud
+def todo_detail(request,id):
+    todo=Todo.objects.get(id=id)
+    context={
+        "todo":todo
+    }
+    return render(request,"todo_detail.html",context)
+
+
+def todo_create(request):
+    form=TodoForm(request.POST or None)
+    if form.is_valid():
+        #name=form.cleaned_data['name'] #1
+        #due_date=form.cleaned_data['due_date'] #2
+        #print(name,due_date) #3
+        #new_todo=Todo.objects.create(name=name,due_date=due_date) #4
+        #the tasks done by lines 1,2,4 can be abstracted to just one line as below ,this is bcz we are using model form from django forms library
+        form.save()  # it will create a Todo instance
+        return redirect('/')
+        
+    context={"form":form}
+    return render(request,"todo_create.html",context)
+
+def todo_update(request,id):
+    todo=Todo.objects.get(id=id)
+    form=TodoForm(request.POST or None,instance=todo)
+    if form.is_valid():
+        form.save()  # it will create a Todo instance
+        return redirect('/')
+        
+    context={"form":form}
+    return render(request,"todo_update.html",context)
+
+def todo_delete(request,id):
+    todo=Todo.objects.get(id=id)
+    todo.delete()
+    return redirect('/')
