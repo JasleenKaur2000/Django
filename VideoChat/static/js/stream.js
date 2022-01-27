4648337ff07ae71eab9ba124030302da386fe223
JasleenKaur2000 <jasleenkaur46353@gmail.com>
2022-01-28 02:26:05 +0530

    generated agora app tokens and saved in js file, added js to room.html

---
+++ VideoChat/static/js/stream.js
@@ -0,0 +1,6 @@
+const APP_ID = "db0e302e89604e739383d9c959d9f5d0";
+const CHANNEL = "main";
+const TOKEN =
+  "006db0e302e89604e739383d9c959d9f5d0IAAeHdlyLoj8yZSrXvp+LzcVAS1/iKUXnEV546iewHT76GTNKL8AAAAAEAD1z9KPwVb0YQEAAQC+VvRh";
+
+console.log("Stream.js connected");
